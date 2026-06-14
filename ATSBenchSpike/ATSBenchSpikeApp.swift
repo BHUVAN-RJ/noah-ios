972af5253c37e6942aeb76ccd57d6dcd20aa9b5b
Bhuvan <bhuvanrj73@gmail.com>
2026-06-14 16:32:20 -0700

    Initial Commit

---
+++ ATSBenchSpike/ATSBenchSpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATSBenchSpikeApp.swift
+//  ATSBenchSpike
+//
+//  Created by Bhuvan R J on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ATSBenchSpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
